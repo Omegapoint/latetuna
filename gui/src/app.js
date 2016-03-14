48b763059d37d10564efbbd1f3493ff634e4acc1
Samuel Moritz <samuel.moritz@gmail.com>
2016-03-14 20:28:43 +0100

    Working React

--- gui/src/app.js
+++ gui/src/app.js
@@ -1,3 +1,13 @@
-var foo = () => console.log("yo");
-
-foo();
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
