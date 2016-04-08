3c1649451b017eaff59b5ac5bac32779ff89ed0f
Samuel Moritz <samuel.moritz@gmail.com>
2016-04-08 15:43:59 +0200

    Conference depends on EventClient

--- conference/src/main/java/com/omegapoint/latetuna/conference/resource/ConferenceController.java
+++ conference/src/main/java/com/omegapoint/latetuna/conference/resource/ConferenceController.java
@@ -1,5 +1,7 @@
 package com.omegapoint.latetuna.conference.resource;
 
+import com.omegapoint.latetuna.EventClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,9 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 public class ConferenceController {
+
+    @Autowired
+    private EventClient eventClient;
 
     @RequestMapping(value = "/", method = GET)
     public String list() {
