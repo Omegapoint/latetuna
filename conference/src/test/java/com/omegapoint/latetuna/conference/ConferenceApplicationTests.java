d330f6c4af0b5360d3395d50745052d5247fd847
Mattias Sällström <mattias.sallstrom@gmail.com>
2016-05-13 16:34:24 +0200

    Fixed the application test

--- conference/src/test/java/com/omegapoint/latetuna/conference/ConferenceApplicationTests.java
+++ conference/src/test/java/com/omegapoint/latetuna/conference/ConferenceApplicationTests.java
@@ -3,17 +3,18 @@ package com.omegapoint.latetuna.conference;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@ActiveProfiles("test")
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ConferenceApplication.class)
 @WebAppConfiguration
 public class ConferenceApplicationTests {
 
 	@Test
-	@Ignore
 	public void contextLoads() {
 	}
 
