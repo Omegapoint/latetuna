cd1ab101f172ee41f466fec58f7b800a9c39bb74
Samuel Moritz <samuel.moritz@gmail.com>
2016-05-26 21:24:42 +0200

    New POST button in the GUI for creating new conferences. Tested end-to-end.

--- conference/src/main/java/com/omegapoint/latetuna/conference/domain/Conference.java
+++ conference/src/main/java/com/omegapoint/latetuna/conference/domain/Conference.java
@@ -1,5 +1,6 @@
 package com.omegapoint.latetuna.conference.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.UUID;
@@ -12,7 +13,8 @@ public class Conference {
     @JsonProperty
     private String name;
 
-    public Conference(String name) {
+    @JsonCreator
+    public Conference(@JsonProperty("name") String name) {
         this.id = UUID.randomUUID();
         this.name = name;
     }
--- gui/src/app.js
+++ gui/src/app.js
@@ -3,9 +3,15 @@ var CommentBox = React.createClass({
     return (
       <div className="commentBox">
         <p>Hello, conference world! I am a CommentBox.</p>
-        <button>POST Conference</button>
+        <button onClick={this.postConference}>POST Conference</button>
       </div>
     );
+  },
+  postConference: function() {
+      var xhttp = new XMLHttpRequest();
+      xhttp.open("POST", "/conference", true);
+      xhttp.setRequestHeader("Content-type", "application/json");
+      xhttp.send('{"name": "Foo"}');
   }
 });
 ReactDOM.render(
