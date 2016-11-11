41c9895870d04befb24304763ea8f88d858ce385
Markus Örebrand <markus.orebrand@gmail.com>
2016-11-11 14:04:56 +0100

    Three thousand

--- setup.sh
+++ setup.sh
@@ -6,13 +6,13 @@ if [ $# -ne 1 ]; then
 fi
 
 MACHINE_NAME=$1
-GUI_URL="http://localhost:8080"
+GUI_URL="http://localhost:3000"
 
 if [ `uname` = "Darwin" ]; then
     echo "This is MacOS X, doing some setup..."
     eval "$(docker-machine env ${MACHINE_NAME})"
     export KAFKA_ADVERTISED_HOST_NAME=`echo ${DOCKER_HOST} | sed 's/[^0-9]*\([0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\).*/\1/'`
-    GUI_URL="http://${KAFKA_ADVERTISED_HOST_NAME}:8080"
+    GUI_URL="http://${KAFKA_ADVERTISED_HOST_NAME}:3000"
 fi
 
 echo "When started, the GUI can be found here: ${GUI_URL}"
